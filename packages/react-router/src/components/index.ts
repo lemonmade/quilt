d828c2079d75545db3081e7968d722bb9c49000f
Chris Sauve <chrismsauve@gmail.com>
2020-03-20 09:37:25 -0400

    Expose prefetcher

--- packages/react-router/src/components/index.ts
+++ packages/react-router/src/components/index.ts
@@ -1,5 +1,6 @@
 export {Link} from './Link';
 export {NavigationBlock} from './NavigationBlock';
+export {Prefetcher} from './Prefetcher';
 export {Redirect} from './Redirect';
 export {Route} from './Route';
 export {Router} from './Router';
