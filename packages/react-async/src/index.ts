5241acef25062e18cb87a3d6941ccf8a901c53b8
Chris Sauve <chrismsauve@gmail.com>
2020-03-28 18:45:28 -0400

    Undo some bad merges

--- packages/react-async/src/index.ts
+++ packages/react-async/src/index.ts
@@ -1,21 +1,18 @@
-export {createResolver, Resolver, ResolverOptions} from '@quilted/async';
+export {createResolver} from '@quilted/async';
+export type {Resolver, ResolverOptions} from '@quilted/async';
 
-export {
+export type {
   AssetTiming,
   DeferTiming,
   AsyncComponentType,
   AsyncHookTarget,
 } from './types';
-export {createAsyncComponent} from './component';
 export {
   useAsync,
   useAsyncAsset,
   usePreload,
   usePrefetch,
   useKeepFresh,
-  Preloadable,
-  Prefetchable,
-  KeepFreshable,
 } from './hooks';
 export type {Preloadable, Prefetchable, KeepFreshable} from './hooks';
 export {createAsyncComponent} from './component';
