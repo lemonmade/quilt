af5c014b809f04e8236d1df6d90584c3928d2f3b
Chris Sauve <chrismsauve@gmail.com>
2020-08-24 17:17:31 -0400

    Sort variables in cache key generation

--- packages/graphql/src/utilities/cache.ts
+++ packages/graphql/src/utilities/cache.ts
@@ -1,8 +1,20 @@
 import type {GraphQLOperation} from '../types';
 
-export function cacheKey<Data = unknown, Variables = {}>(
+export function cacheKey<Data, Variables>(
   {id}: GraphQLOperation<Data, Variables>,
   variables?: Variables,
 ) {
-  return `${id}${variables ? JSON.stringify(variables) : '{}'}`;
+  return `${id}${JSON.stringify(variables ? sortVariables(variables) : {})}`;
+}
+
+function sortVariables<T>(variables: T) {
+  const newVariables: T = {} as any;
+
+  for (const sortedKey of Object.keys(variables).sort((keyOne, keyTwo) =>
+    keyOne.localeCompare(keyTwo),
+  ) as (keyof T)[]) {
+    newVariables[sortedKey] = variables[sortedKey];
+  }
+
+  return newVariables;
 }
