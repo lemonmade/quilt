7639e6c14595819eeb8f41a888b1c14a7f095dc2
Chris Sauve <chrismsauve@gmail.com>
2020-12-21 22:20:34 -0500

    Add quilt/http entrypoint

--- packages/quilt/sewing-kit.config.ts
+++ packages/quilt/sewing-kit.config.ts
@@ -3,6 +3,7 @@ import {quiltPackage} from '../../config/sewing-kit';
 
 export default createPackage((pkg) => {
   pkg.entry({root: './src/index'});
+  pkg.entry({root: './src/http', name: 'http'});
   pkg.entry({root: './src/html', name: 'html'});
   pkg.entry({root: './src/server', name: 'server', runtime: Runtime.Node});
   pkg.entry({root: './src/testing', name: 'testing', runtime: Runtime.Node});
