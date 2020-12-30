c7564d2ced3f8d7787dea89443e7309335dc0a3d
Chris Sauve <chrismsauve@gmail.com>
2020-12-30 11:39:28 -0500

    Fix missing polyfills

--- packages/sewing-kit-plugins/src/polyfills.ts
+++ packages/sewing-kit-plugins/src/polyfills.ts
@@ -2,6 +2,7 @@ import {createProjectPlugin, Runtime, TargetRuntime} from '@sewing-kit/plugins';
 import type {WebApp, Service} from '@sewing-kit/plugins';
 import type {BuildWebAppConfigurationHooks} from '@sewing-kit/hooks';
 import {mappedPolyfillsForEnv} from '@quilted/polyfills';
+import {updateSewingKitBabelPreset} from '@sewing-kit/plugin-javascript';
 import type {} from '@sewing-kit/plugin-webpack';
 import type {} from '@sewing-kit/plugin-jest';
 
@@ -24,6 +25,10 @@ export function polyfills({features}: {features?: PolyfillFeature[]} = {}) {
                 ),
               ]);
             }
+
+            configuration.babelConfig?.hook(
+              updateSewingKitBabelPreset({polyfill: 'usage'}),
+            );
 
             configuration.webpackAliases?.hook(async (aliases) => {
               let environment: string | string[] | undefined;
@@ -57,6 +62,10 @@ export function polyfills({features}: {features?: PolyfillFeature[]} = {}) {
               ),
             ]);
           }
+
+          configuration.babelConfig?.hook(
+            updateSewingKitBabelPreset({polyfill: 'usage'}),
+          );
 
           configuration.webpackAliases?.hook(async (aliases) => {
             let environment: string | string[] | undefined;
