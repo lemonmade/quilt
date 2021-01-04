7e872504cf7ba15a7015df4a1f9288102b812de1
Chris Sauve <chrismsauve@gmail.com>
2021-01-03 19:51:30 -0500

    Replace use-subscription with a module-friendly version

---
+++ packages/use-subscription/README.md
@@ -0,0 +1,3 @@
+# `@quilted/use-subscription`
+
+Fork of https://github.com/facebook/react/tree/master/packages/use-subscription that can be used as a module.
---
+++ packages/use-subscription/src/index.ts
@@ -0,0 +1,71 @@
+import {useDebugValue, useEffect, useState} from 'react';
+
+interface Options<T> {
+  getCurrentValue: () => T;
+  subscribe: (callback: () => void) => () => void;
+}
+
+export function useSubscription<T>({
+  getCurrentValue,
+  subscribe,
+}: Options<T>): T {
+  const [state, setState] = useState(() => ({
+    getCurrentValue,
+    subscribe,
+    value: getCurrentValue(),
+  }));
+
+  let valueToReturn = state.value;
+
+  if (
+    state.getCurrentValue !== getCurrentValue ||
+    state.subscribe !== subscribe
+  ) {
+    valueToReturn = getCurrentValue();
+
+    setState({
+      getCurrentValue,
+      subscribe,
+      value: valueToReturn,
+    });
+  }
+
+  useDebugValue(valueToReturn);
+
+  useEffect(() => {
+    let didUnsubscribe = false;
+
+    const checkForUpdates = () => {
+      if (didUnsubscribe) {
+        return;
+      }
+
+      const value = getCurrentValue();
+
+      setState((prevState) => {
+        if (
+          prevState.getCurrentValue !== getCurrentValue ||
+          prevState.subscribe !== subscribe
+        ) {
+          return prevState;
+        }
+
+        if (prevState.value === value) {
+          return prevState;
+        }
+
+        return {...prevState, value};
+      });
+    };
+    const unsubscribe = subscribe(checkForUpdates);
+
+    checkForUpdates();
+
+    return () => {
+      didUnsubscribe = true;
+      unsubscribe();
+    };
+  }, [getCurrentValue, subscribe]);
+
+  return valueToReturn;
+}
