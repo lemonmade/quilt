77baf247dd676ce6fdc006d3d828d1620468a313
Chris Sauve <chrismsauve@gmail.com>
2021-04-15 16:47:26 -0400

    Fix cookie joining in aws http-handler

--- packages/aws/src/http-handlers.ts
+++ packages/aws/src/http-handlers.ts
@@ -13,7 +13,7 @@ export function createLambdaApiGatewayProxy(
 
     const headers = new Headers(event.headers as Record<string, string>);
 
-    const cookies = Cookies.parse(event.cookies?.join(', ') ?? '');
+    const cookies = Cookies.parse(event.cookies?.join('; ') ?? '');
 
     const response =
       (await handler.run({
