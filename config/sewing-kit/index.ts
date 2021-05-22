18571d5a75d7e494c97acfcda7c262e97452ad0f
Chris Sauve <chrismsauve@gmail.com>
2021-05-22 15:17:31 -0400

    Fix issues with binaries

--- config/sewing-kit/index.ts
+++ config/sewing-kit/index.ts
@@ -1,3 +1,4 @@
+import {relative, dirname} from 'path';
 import {
   Package,
   createComposedProjectPlugin,
@@ -5,6 +6,7 @@ import {
   createProjectBuildPlugin,
   createProjectPlugin,
 } from '@sewing-kit/plugins';
+import type {ProjectPluginContext} from '@sewing-kit/plugins';
 
 import {react} from '@sewing-kit/plugin-react';
 import {javascript, updateBabelPreset} from '@sewing-kit/plugin-javascript';
@@ -24,7 +26,9 @@ export function quiltPackage() {
     packageBuild({
       browserTargets: 'last 2 versions',
       nodeTargets: 'node 12',
+      binaries: false,
     }),
+    buildFixedBinaries(),
     createProjectBuildPlugin('Quilt.ExternalSelf', ({hooks}) => {
       hooks.target.hook(({hooks}) => {
         hooks.configure.hook(({rollupExternal, rollupPlugins}) => {
@@ -52,6 +56,73 @@ export function quiltPackage() {
       });
     }),
   ]);
+}
+
+// Fixes an issue in the skn version that points at ../build/node
+function buildFixedBinaries() {
+  return createProjectBuildPlugin<Package>(
+    'Quilt.BuildBinaries',
+    ({hooks, project, api}) => {
+      hooks.steps.hook((steps) =>
+        project.binaries.length > 0
+          ? [...steps, createWriteBinariesStep({project, api})]
+          : steps,
+      );
+    },
+  );
+}
+
+function createWriteBinariesStep({
+  project,
+  api,
+}: Pick<ProjectPluginContext<Package>, 'project' | 'api'>) {
+  const binaryCount = project.binaries.length;
+  const sourceRoot = project.fs.resolvePath('src');
+
+  return api.createStep(
+    {
+      id: 'PackageBinaries.WriteBinaries',
+      label:
+        binaryCount === 1 ? 'write binary' : `write ${binaryCount} binaries`,
+    },
+    async (step) => {
+      await Promise.all(
+        project.binaries.map(async ({name, root, aliases = []}) => {
+          const relativeFromSourceRoot = relative(
+            sourceRoot,
+            project.fs.resolvePath(root),
+          );
+
+          const destinationInOutput = project.fs.buildPath(
+            'cjs',
+            relativeFromSourceRoot,
+          );
+
+          for (const binaryName of [name, ...aliases]) {
+            const binaryFile = project.fs.resolvePath('bin', binaryName);
+            const relativeFromBinary = normalizedRelative(
+              dirname(binaryFile),
+              destinationInOutput,
+            );
+
+            await project.fs.write(
+              binaryFile,
+              `#!/usr/bin/env node\nrequire(${JSON.stringify(
+                relativeFromBinary,
+              )})`,
+            );
+
+            await step.exec('chmod', ['+x', binaryFile]);
+          }
+        }),
+      );
+    },
+  );
+}
+
+function normalizedRelative(from: string, to: string) {
+  const rel = relative(from, to);
+  return rel.startsWith('.') ? rel : `./${rel}`;
 }
 
 export function terser({
--- packages/create-quilt-project/sewing-kit.config.ts
+++ packages/create-quilt-project/sewing-kit.config.ts
@@ -3,6 +3,6 @@ import {quiltPackage} from '../../config/sewing-kit';
 
 export default createPackage((pkg) => {
   pkg.runtime(Runtime.Node);
-  pkg.binary({name: 'create-quilt-project', root: './src/index', aliases: []});
+  pkg.binary({name: 'create-quilt-project', root: './src/index'});
   pkg.use(quiltPackage());
 });
