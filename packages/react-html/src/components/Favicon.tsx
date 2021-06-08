7c3579cb7a26c24b6a123808c42682f9f910c1be
Chris Sauve <chrismsauve@gmail.com>
2021-06-08 01:10:41 -0400

    Works everywhere other than Safari, good enough

--- packages/react-html/src/components/Favicon.tsx
+++ packages/react-html/src/components/Favicon.tsx
@@ -5,14 +5,19 @@ export type Props =
       source: string;
       type?: string;
       empty?: never;
+      emoji?: never;
     }
-  | {empty: true; source?: never; type?: never};
+  | {emoji: string; empty?: never; source?: never; type?: never}
+  | {empty: true; emoji?: never; source?: never; type?: never};
 
-export function Favicon({empty, type, source}: Props) {
+export function Favicon({empty, emoji, type, source}: Props) {
   let resolvedSource: string;
 
   if (empty) {
     resolvedSource = 'data:image';
+  } else if (emoji) {
+    // Hat tip: https://twitter.com/LeaVerou/status/1241619866475474946
+    resolvedSource = `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>${emoji}</text></svg>`;
   } else {
     resolvedSource = source!;
   }
