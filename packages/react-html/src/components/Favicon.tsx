dc2c9810320c2c3db37b7af0ba6d4d494895f30b
Chris Sauve <chrismsauve@gmail.com>
2021-06-07 21:54:42 -0400

    Fix name of emoji prop

--- packages/react-html/src/components/Favicon.tsx
+++ packages/react-html/src/components/Favicon.tsx
@@ -5,19 +5,19 @@ export type Props =
       source: string;
       type?: string;
       empty?: never;
-      favicon?: never;
+      emoji?: never;
     }
-  | {favicon: string; empty?: never; source?: never; type?: never}
-  | {empty: true; favicon?: never; source?: never; type?: never};
+  | {emoji: string; empty?: never; source?: never; type?: never}
+  | {empty: true; emoji?: never; source?: never; type?: never};
 
-export function Favicon({empty, favicon, type, source}: Props) {
+export function Favicon({empty, emoji, type, source}: Props) {
   let resolvedSource: string;
 
   if (empty) {
     resolvedSource = 'data:image';
-  } else if (favicon) {
+  } else if (emoji) {
     // Hat tip: https://twitter.com/LeaVerou/status/1241619866475474946
-    resolvedSource = `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>${favicon}</text></svg>`;
+    resolvedSource = `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>${emoji}</text></svg>`;
   } else {
     resolvedSource = source!;
   }
