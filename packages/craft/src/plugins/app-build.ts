c1249e1c78e53a58528469202dffca2b45f17ccc
Chris Sauve <chrismsauve@gmail.com>
2021-06-11 22:26:25 -0400

    Add basic vendor modules

--- packages/craft/src/plugins/app-build.ts
+++ packages/craft/src/plugins/app-build.ts
@@ -1,3 +1,4 @@
+import type {GetModuleInfo, GetManualChunk} from 'rollup';
 import {createProjectPlugin} from '@quilted/sewing-kit';
 import type {App} from '@quilted/sewing-kit';
 
@@ -30,9 +31,10 @@ export function appBuild() {
           ...outputs,
           {
             format: 'esm',
-            entryFileNames: `[name].[hash].js`,
+            entryFileNames: `app.[hash].js`,
             assetFileNames: `[name].[hash].[ext]`,
             chunkFileNames: `[name].[hash].js`,
+            manualChunks: createManualChunksSorter(),
             dir: await outputDirectory.run(
               workspace.fs.buildPath(
                 workspace.apps.length > 1 ? `apps/${project.name}` : 'app',
@@ -59,4 +61,94 @@ export function appBuild() {
       );
     },
   });
+}
+
+const FRAMEWORK_CHUNK_NAME = 'framework';
+const VENDOR_CHUNK_NAME = 'vendor';
+const FRAMEWORK_TEST_STRINGS = [
+  '/node_modules/preact/',
+  '/node_modules/react/',
+  '/node_modules/@quilted/',
+];
+
+// When building from source, quilt packages are not in node_modules,
+// so we instead add their repo paths to the list of framework test strings.
+if (process.env.SEWING_KIT_FROM_SOURCE) {
+  FRAMEWORK_TEST_STRINGS.push('/quilt/packages/');
+}
+
+interface ImportMetadata {
+  fromEntry: boolean;
+  fromFramework: boolean;
+}
+
+// Inspired by Vite: https://github.com/vitejs/vite/blob/c69f83615292953d40f07b1178d1ed1d72abe695/packages/vite/src/node/build.ts#L567
+function createManualChunksSorter(): GetManualChunk {
+  const cache = new Map<string, ImportMetadata>();
+
+  return (id, {getModuleInfo}) => {
+    if (
+      !id.includes('node_modules') &&
+      !FRAMEWORK_TEST_STRINGS.some((test) => id.includes(test))
+    ) {
+      return;
+    }
+
+    if (id.endsWith('.css')) return;
+
+    const importMetadata = getImportMetadata(id, getModuleInfo, cache);
+
+    if (!importMetadata.fromEntry) return;
+
+    if (importMetadata.fromFramework) {
+      return FRAMEWORK_CHUNK_NAME;
+    }
+
+    return VENDOR_CHUNK_NAME;
+  };
+}
+
+function getImportMetadata(
+  id: string,
+  getModuleInfo: GetModuleInfo,
+  cache: Map<string, ImportMetadata>,
+  importStack: string[] = [],
+): ImportMetadata {
+  if (cache.has(id)) return cache.get(id)!;
+
+  if (importStack.includes(id)) {
+    // circular dependencies
+    const result = {fromEntry: false, fromFramework: false};
+    cache.set(id, result);
+    return result;
+  }
+
+  const module = getModuleInfo(id);
+
+  if (!module) {
+    const result = {fromEntry: false, fromFramework: false};
+    cache.set(id, result);
+    return result;
+  }
+
+  if (module.isEntry) {
+    const result = {fromEntry: true, fromFramework: false};
+    cache.set(id, result);
+    return result;
+  }
+
+  const newImportStack = [...importStack, id];
+  const importersMetadata = module.importers.map((importer) =>
+    getImportMetadata(importer, getModuleInfo, cache, newImportStack),
+  );
+
+  const result = {
+    fromEntry: importersMetadata.some(({fromEntry}) => fromEntry),
+    fromFramework:
+      FRAMEWORK_TEST_STRINGS.some((test) => id.includes(test)) ||
+      importersMetadata.some((importer) => importer.fromFramework),
+  };
+
+  cache.set(id, result);
+  return result;
 }
