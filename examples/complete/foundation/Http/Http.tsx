4326e6fd5e4d05712acd7fd6230cf67d4f9ac635
Chris Sauve <chrismsauve@gmail.com>
2021-07-01 00:56:09 -0400

    Disable FLoC in example

--- examples/complete/foundation/Http/Http.tsx
+++ examples/complete/foundation/Http/Http.tsx
@@ -1,9 +1,17 @@
-import {CacheControl} from '@quilted/quilt/http';
+import {CacheControl, ResponseHeader} from '@quilted/quilt/http';
 
 export function Http() {
   return (
     <>
       <CacheControl cache={false} />
+
+      {/**
+       * Disables Google’s Federated Learning of Cohorts (“FLoC”)
+       * tracking initiative.
+       *
+       * @see https://www.eff.org/deeplinks/2021/03/googles-floc-terrible-idea
+       */}
+      <ResponseHeader name="Permissions-Policy" value="interest-cohort=()" />
     </>
   );
 }
