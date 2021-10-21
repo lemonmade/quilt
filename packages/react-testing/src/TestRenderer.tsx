001af846154a952b5585da8543ced88eebf33d54
Chris Sauve <chrismsauve@gmail.com>
2021-10-21 16:24:22 -0400

    Throw render errors in testing library

--- packages/react-testing/src/TestRenderer.tsx
+++ packages/react-testing/src/TestRenderer.tsx
@@ -3,6 +3,7 @@ import type {ReactElement, ReactNode} from 'react';
 
 interface State<ComponentProps> {
   props?: Partial<ComponentProps>;
+  error?: Error;
 }
 
 interface Props<ComponentProps> {
@@ -16,14 +17,25 @@ export class TestRenderer<ComponentProps> extends Component<
   Props<ComponentProps>,
   State<ComponentProps>
 > {
+  static getDerivedStateFromError(error: any) {
+    return {error};
+  }
+
   state: State<ComponentProps> = {};
 
   setProps(props: Partial<ComponentProps>) {
     this.setState({props});
   }
 
+  getError() {
+    return this.state.error;
+  }
+
   render() {
-    const {props} = this.state;
+    const {props, error} = this.state;
+
+    if (error) return null;
+
     const {children, render = defaultRender} = this.props;
     return render(
       props ? (cloneElement(children as any, props) as any) : children!,
---
+++ packages/react-testing/src/tests/e2e.test.tsx
@@ -0,0 +1,58 @@
+/* eslint no-console: off */
+
+import {useState} from 'react';
+import {mount} from '..';
+
+describe('e2e', () => {
+  let consoleError = console.error;
+
+  beforeEach(() => {
+    consoleError = console.error;
+
+    // React logs these errors no matter what, even though we
+    // catch the error in the test runner.
+    console.error = (...args: any[]) => {
+      if (
+        typeof args[0] === 'string' &&
+        args[0].includes('<ThrowingComponent>')
+      ) {
+        return;
+      }
+
+      return consoleError.call(console, ...args);
+    };
+  });
+
+  afterEach(() => {
+    console.error = consoleError;
+  });
+
+  it('throws if an React component throws during mount', () => {
+    const error = new Error('oh no!');
+
+    function ThrowingComponent() {
+      throw error;
+      return null;
+    }
+
+    expect(() => mount(<ThrowingComponent />)).toThrowError(error);
+  });
+
+  it('throws if a React component throws after an update', () => {
+    const error = new Error('oh no!');
+
+    function ThrowingComponent() {
+      const [shouldThrow, setShouldThrow] = useState(false);
+
+      if (shouldThrow) throw error;
+
+      return <button onClick={() => setShouldThrow(true)}>Throw!</button>;
+    }
+
+    const throwingComponent = mount(<ThrowingComponent />);
+
+    expect(() =>
+      throwingComponent.find('button')!.trigger('onClick'),
+    ).toThrowError(error);
+  });
+});
