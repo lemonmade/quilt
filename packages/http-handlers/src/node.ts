7cbcfc4c3b97c7b60fa4ffee883d0897503f6617
Chris Sauve <chrismsauve@gmail.com>
2022-03-09 18:21:31 -0500

    Add additional node utility functions to http-handlers package

--- packages/http-handlers/src/node.ts
+++ packages/http-handlers/src/node.ts
@@ -1,5 +1,5 @@
 import {createServer} from 'http';
-import type {RequestListener} from 'http';
+import type {RequestListener, IncomingMessage, ServerResponse} from 'http';
 import send from 'send';
 import type {SendStream} from 'send';
 import {URL} from 'url';
@@ -7,7 +7,7 @@ import {URL} from 'url';
 import {createHeaders} from '@quilted/http';
 
 import {notFound} from './response';
-import type {HttpHandler} from './types';
+import type {HttpHandler, Response, RequestOptions} from './types';
 
 export function createHttpServer(handler: HttpHandler) {
   return createServer(createHttpRequestListener(handler));
@@ -56,7 +56,7 @@ export function createHttpRequestListener(
                 Array.isArray(value) ? value.join(',') : value ?? '',
               ],
             ),
-          ) as any,
+          ),
         })) ?? notFound();
 
       const {status, headers, cookies, body: resultBody} = result;
@@ -136,4 +136,72 @@ export function serveStatic(root: string, {baseUrl = '/'}: StaticOptions = {}) {
   };
 
   return listener;
+}
+
+export async function transformRequest(
+  request: IncomingMessage,
+): Promise<RequestOptions> {
+  const body = await new Promise<string>((resolve, reject) => {
+    let data = '';
+
+    request.on('data', (chunk) => {
+      data += String(chunk);
+    });
+
+    request.on('end', () => {
+      resolve(data);
+    });
+
+    request.on('close', () => {
+      reject();
+    });
+  });
+
+  const forwardedProtocolHeader = request.headers['x-forwarded-proto'];
+  const forwardedProtocol = Array.isArray(forwardedProtocolHeader)
+    ? forwardedProtocolHeader[0]
+    : forwardedProtocolHeader?.split(/\s*,\s*/)[0];
+
+  return {
+    body,
+    method: request.method!,
+    url: new URL(
+      request.url!,
+      `${forwardedProtocol ?? 'http'}://${
+        request.headers['x-forwarded-host'] ?? request.headers.host
+      }`,
+    ),
+    headers: createHeaders(
+      Object.entries(request.headers).map<[string, string]>(
+        ([header, value]) => [
+          header,
+          Array.isArray(value) ? value.join(',') : value ?? '',
+        ],
+      ),
+    ),
+  };
+}
+
+export function applyResponse(
+  response: Response,
+  httpResponse: ServerResponse,
+) {
+  const {status, headers, cookies, body: resultBody} = response;
+  const setCookieHeaders = cookies.getAll();
+
+  httpResponse.writeHead(
+    status,
+    [...headers].reduce<Record<string, string | string[]>>(
+      (allHeaders, [key, value]) => {
+        if (key.toLowerCase() === 'set-cookie') return allHeaders;
+
+        allHeaders[key] = value;
+        return allHeaders;
+      },
+      setCookieHeaders ? {'Set-Cookie': setCookieHeaders} : {},
+    ),
+  );
+
+  if (resultBody != null) httpResponse.write(resultBody);
+  httpResponse.end();
 }
