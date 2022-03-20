d3c6346dc61a77fd6d45f13abe29222742b895fc
Chris Sauve <chrismsauve@gmail.com>
2022-03-20 00:51:36 -0400

    Fix some React types (#206)

--- packages/quilt/src/react/index.ts
+++ packages/quilt/src/react/index.ts
@@ -1,2 +1,4 @@
+// @ts-expect-error This module is not imported for types
 export * from '@quilted/react';
+// @ts-expect-error This module is not imported for types
 export {default} from '@quilted/react';
--- packages/quilt/src/react/server.ts
+++ packages/quilt/src/react/server.ts
@@ -1,1 +1,2 @@
+// @ts-expect-error This module is never imported for types
 export * from '@quilted/react/server';
