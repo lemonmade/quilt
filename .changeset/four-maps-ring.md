433f94f56a86a687b5f70a2887a83a3aae25e025
Chris Sauve <chrismsauve@gmail.com>
2022-03-21 20:54:10 -0400

    Add missing quilted/quilt/crypto entry

---
+++ .changeset/four-maps-ring.md
@@ -0,0 +1,5 @@
+---
+'@quilted/quilt': patch
+---
+
+Add missing quilted/quilt/crypto entry
--- packages/quilt/sewing-kit.config.ts
+++ packages/quilt/sewing-kit.config.ts
@@ -29,6 +29,7 @@ export default createPackage((pkg) => {
   });
   pkg.entry({source: './src/polyfills/base', name: 'polyfills/base'});
   pkg.entry({source: './src/polyfills/fetch', name: 'polyfills/fetch'});
+  pkg.entry({source: './src/polyfills/crypto', name: 'polyfills/crypto'});
   pkg.entry({source: './src/polyfills/noop', name: 'polyfills/noop'});
   pkg.entry({source: './src/server', name: 'server', runtime: Runtime.Node});
   pkg.entry({source: './src/static', name: 'static', runtime: Runtime.Node});
