f273e224761c23eac26abec1fffc270a6a97bc08
Chris Sauve <chrismsauve@gmail.com>
2022-03-23 18:13:02 -0400

    More preact-render-to-string workarounds...

--- packages/react/src/server.ts
+++ packages/react/src/server.ts
@@ -1,5 +1,12 @@
 // Originally, I wanted to just re-export `preact/compat/server`,
 // but it lists export conditions in the wrong order.
 // @see https://github.com/preactjs/preact/issues/3488
-export * from 'preact-render-to-string';
-export {default} from 'preact-render-to-string';
+
+// I also wanted to export the named exports from this package, but
+// unfortunately those are busted in the CommonJS version of the
+// library.
+// @see https://github.com/preactjs/preact-render-to-string/issues/197
+export {
+  default as renderToString,
+  default as renderToStaticMarkup,
+} from 'preact-render-to-string';
