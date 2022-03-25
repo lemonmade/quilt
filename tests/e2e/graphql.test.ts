2fe511f92ad0fc3a84c4594a7eb41ab20589315b
Chris Sauve <chrismsauve@gmail.com>
2022-03-25 02:08:19 -0400

    E2E test

---
+++ tests/e2e/graphql.test.ts
@@ -0,0 +1,59 @@
+import {stripIndent, withWorkspace} from './utilities';
+
+jest.setTimeout(20_000);
+
+describe('graphql', () => {
+  describe('type generation', () => {
+    it('generates types from a project’s GraphQL config file', async () => {
+      await withWorkspace(async (workspace) => {
+        await workspace.fs.write({
+          'graphql.config.mjs': stripIndent`
+            export default {
+              schema: 'schema.graphql',
+              documents: 'features/**/*.graphql',
+            };
+          `,
+          'schema.graphql': stripIndent`
+            type Query {
+              title: String!
+            }
+
+            schema {
+              query: Query
+            }
+          `,
+          'foundation/Routes.tsx': stripIndent`
+            import {useRoutes} from '@quilted/quilt';
+            import {Start} from '../features/Start';
+            
+            export function Routes() {
+              return useRoutes([{match: '/', render: () => <Start />}]);
+            }
+          `,
+          'features/Start/index.tsx': stripIndent`
+            import {useQuery} from '@quilted/quilt';
+            import startQuery from './StartQuery.graphql';
+
+            export function Start() {
+              const {data} = useQuery(startQuery);
+              if (data == null) return null;
+
+              const title: string = data.title;
+              return <div>Title is: {title}</div>;
+            }
+          `,
+          'features/Start/StartQuery.graphql': stripIndent`
+            query Start {
+              title
+            }
+          `,
+        });
+
+        const result = await workspace.command.sewingKit.typeCheck();
+
+        console.log(result);
+        expect(result).toBeTruthy();
+      });
+    });
+  });
+});
