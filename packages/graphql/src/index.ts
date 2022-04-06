7b3237510da145160f04d30018658abd27d716e3
Chris Sauve <chrismsauve@gmail.com>
2022-04-05 22:28:42 -0400

    Fix GraphQL type export

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -1,4 +1,5 @@
-export {GraphQL, createGraphQL} from './client';
+export {createGraphQL} from './client';
+export type {GraphQL} from './client';
 export {createHttpFetch} from './fetch';
 export type {HttpFetchContext, HttpFetchOptions} from './fetch';
 export {cacheKey} from './utilities/cache';
