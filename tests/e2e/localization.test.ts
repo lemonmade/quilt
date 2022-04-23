b53fabfab59d2b87a6427cc27eb73af6c011a09d
Chris Sauve <chrismsauve@gmail.com>
2022-04-23 00:19:33 -0400

    Fix localization e2e tests

--- tests/e2e/localization.test.ts
+++ tests/e2e/localization.test.ts
@@ -130,7 +130,7 @@ describe('localization', () => {
 
         const {page} = await buildAppAndOpenPage(workspace, {
           path: '/fr',
-          locale: 'fr-CA',
+          locale: 'en',
         });
 
         expect(
@@ -139,6 +139,67 @@ describe('localization', () => {
           ),
         ).toBe('fr');
         expect(await page.textContent('body')).toBe('Locale: fr');
+      });
+    });
+
+    it('can localize by route while preserving a regional locale', async () => {
+      await withWorkspace({fixture: 'basic-app'}, async (workspace) => {
+        const {fs} = workspace;
+
+        await fs.write({
+          'App.tsx': stripIndent`
+            import {AppContext, PerformanceContext, LocalizedRouter, createRoutePathLocalization} from '@quilted/quilt';
+
+            import {Http} from './foundation/Http';
+            import {Head} from './foundation/Head';
+            import {Routes} from './foundation/Routes';
+
+            const localization = createRoutePathLocalization({
+              default: 'en',
+              locales: ['en', 'fr'],
+            })
+            
+            export default function App() {
+              return (
+                <AppContext>
+                  <LocalizedRouter localization={localization}>
+                    <PerformanceContext>
+                      <Http />
+                      <Head />
+                      <Routes />
+                    </PerformanceContext>
+                  </LocalizedRouter>
+                </AppContext>
+              );
+            }
+          `,
+        });
+
+        await fs.write({
+          'foundation/Routes.tsx': stripIndent`
+            import {useLocale} from '@quilted/quilt';
+            
+            export function Routes() {
+              const locale = useLocale();
+
+              return (
+                <div>Locale: {locale}</div>
+              );
+            }
+          `,
+        });
+
+        const {page} = await buildAppAndOpenPage(workspace, {
+          path: '/fr',
+          locale: 'fr-CA',
+        });
+
+        expect(
+          await page.evaluate(() =>
+            document.documentElement.getAttribute('lang'),
+          ),
+        ).toBe('fr-CA');
+        expect(await page.textContent('body')).toBe('Locale: fr-CA');
       });
     });
   });
