b265c4569ca645c585214097a9e149842d4fa83c
Chris Sauve <chrismsauve@gmail.com>
2022-04-25 23:50:13 -0400

    Fix some example app patterns

--- examples/basic-app/app/features/Start/Start.tsx
+++ examples/basic-app/app/features/Start/Start.tsx
@@ -1,14 +1,5 @@
-import {useCookie, useCurrentUrl} from '@quilted/quilt';
-
 import styles from './Start.module.css';
 
-export default function Start() {
-  const currentUrl = useCurrentUrl();
-  const user = useCookie('user');
-
-  return (
-    <div className={styles.Start}>
-      Hello world!!! Route: {currentUrl.pathname}, User: {user ?? 'no user'}
-    </div>
-  );
+export function Start() {
+  return <div className={styles.Start}>Hello world!!!</div>;
 }
