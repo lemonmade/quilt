13d42f81c181ff761acf5c200d8fe55d7f9c95f8
Chris Sauve <chrismsauve@gmail.com>
2022-05-16 23:19:14 -0400

    Fix lint

--- tests/e2e/polyfills.test.ts
+++ tests/e2e/polyfills.test.ts
@@ -5,64 +5,67 @@ import {withWorkspace, stripIndent, getPort, waitForUrl} from './utilities';
 describe('http', () => {
   describe('cookies', () => {
     it('provides the request cookies during server rendering', async () => {
-      await withWorkspace({fixture: 'basic-api', debug: true}, async (workspace) => {
-        const {fs, command} = workspace;
+      await withWorkspace(
+        {fixture: 'basic-api', debug: true},
+        async (workspace) => {
+          const {fs, command} = workspace;
 
-        await fs.write({
-          'api.ts': stripIndent`
-            import {createHttpHandler, json} from '@quilted/quilt/http-handlers';
+          await fs.write({
+            'api.ts': stripIndent`
+              import {createHttpHandler, json} from '@quilted/quilt/http-handlers';
 
-            const handler = createHttpHandler();
-            
-            handler.get('/', async () => {
-              const result = await fetch('https://swapi-graphql.netlify.app/.netlify/functions/index', {
-                method: 'POST',
-                signal: new AbortController().signal,
-                body: JSON.stringify({
-                  query: '{ __schema { types { name } } }',
-                }),
-                headers: {
-                  'Content-Type': 'application/json',
-                }
+              const handler = createHttpHandler();
+              
+              handler.get('/', async () => {
+                const result = await fetch('https://swapi-graphql.netlify.app/.netlify/functions/index', {
+                  method: 'POST',
+                  signal: new AbortController().signal,
+                  body: JSON.stringify({
+                    query: '{ __schema { types { name } } }',
+                  }),
+                  headers: {
+                    'Content-Type': 'application/json',
+                  }
+                });
+
+                return json(await result.json());
               });
+              
+              export default handler;          
+            `,
+            'quilt.project.ts': stripIndent`
+              import {createService, quiltService, quiltWorkspace} from '@quilted/craft';
+              import {addInternalExportCondition} from '../../common/craft';
+              
+              export default createService((app) => {
+                app.entry('./api');
+                app.use(
+                  quiltWorkspace(),
+                  quiltService({
+                    polyfill: {features: ['fetch', 'abort-controller']},
+                  }),
+                  addInternalExportCondition(),
+                );
+              });
+            `,
+          });
 
-              return json(await result.json());
-            });
-            
-            export default handler;          
-          `,
-          'quilt.project.ts': stripIndent`
-            import {createService, quiltService, quiltWorkspace} from '@quilted/craft';
-            import {addInternalExportCondition} from '../../common/craft';
-            
-            export default createService((app) => {
-              app.entry('./api');
-              app.use(
-                quiltWorkspace(),
-                quiltService({
-                  polyfill: {features: ['fetch', 'abort-controller']},
-                }),
-                addInternalExportCondition(),
-              );
-            });
-          `,
-        });
+          await command.quilt.build();
 
-        await command.quilt.build();
+          const port = await getPort();
+          const url = new URL(`http://localhost:${port}`);
 
-        const port = await getPort();
-        const url = new URL(`http://localhost:${port}`);
+          // Start the server
+          command.node(fs.resolve('build/runtime/index.js'), {
+            env: {PORT: String(port)},
+          });
 
-        // Start the server
-        command.node(fs.resolve('build/runtime/index.js'), {
-          env: {PORT: String(port)},
-        });
+          await waitForUrl(url);
+          const result = await (await fetch(url)).json();
 
-        await waitForUrl(url);
-        const result = await (await fetch(url)).json();
-
-        expect(result).toMatchObject({data: {__schema: expect.any(Object)}});
-      });
+          expect(result).toMatchObject({data: {__schema: expect.any(Object)}});
+        },
+      );
     });
   });
 });
