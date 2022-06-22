26baa273ebcff94c4044c67dca0e092bbb43f612
Chris Sauve <chrismsauve@gmail.com>
2022-06-22 14:41:09 -0400

    Remove examples for now

--- examples/app/features/Start/Start.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './Start.module.css';
-
-export function Start() {
-  return <div className={styles.Start}>Hello world!!!</div>;
-}
--- examples/basic-app/quilt.project.ts
+++
@@ -1,6 +0,0 @@
-import {createApp, quiltApp, quiltWorkspace} from '@quilted/craft';
-
-export default createApp((app) => {
-  app.entry('./app');
-  app.use(quiltWorkspace(), quiltApp());
-});
