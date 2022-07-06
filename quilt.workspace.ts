b7901ef196c500facf251e17464e2780248c6f02
Chris Sauve <chrismsauve@gmail.com>
2022-07-05 21:23:33 -0400

    Fix React aliasing

--- quilt.workspace.ts
+++ quilt.workspace.ts
@@ -20,12 +20,28 @@ export default createWorkspace((workspace) => {
     quiltWorkspace({graphql: false}),
     createWorkspacePlugin({
       name: 'Quilt.WorkspaceExtras',
-      test({configure}) {
+      test({configure, project}) {
         configure(({jestWatchIgnore}) => {
           jestWatchIgnore?.((ignore) => [
             ...ignore,
             path.resolve('tests/e2e/output'),
           ]);
+        });
+
+        project(({configure}) => {
+          configure(({jestModuleMapper}) => {
+            jestModuleMapper?.((moduleMapper) => {
+              const newModuleMapper = {...moduleMapper};
+
+              for (const [key, transform] of Object.entries(moduleMapper)) {
+                if (/^[^]@quilted[/]react(-dom)?([$]|[/])/.test(key)) {
+                  newModuleMapper[key.replace('^@quilted/', '^')] = transform;
+                }
+              }
+
+              return newModuleMapper;
+            });
+          });
         });
       },
     }),
