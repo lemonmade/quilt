6753a58c498bd15753f12d00ff65a2a724db7ad1
Chris Sauve <chrismsauve@gmail.com>
2022-07-21 22:41:33 -0400

    Fix more browser ESM packages

--- packages/craft/jest/resolver.cjs
+++ packages/craft/jest/resolver.cjs
@@ -1,14 +1,24 @@
+// These libraries ship with the browser export condition targeting ESM. This breaks Jest,
+// because it prefers the browser condition but does not understand ESM yet. This deletes the
+// offending package.json fields so that the CommonJS variants are preferred.
+//
+// @see https://github.com/microsoft/accessibility-insights-web/pull/5421#issuecomment-1109168149
+const PACKAGES_WITH_ESM_BROWSER_ENTRIES = new Set([
+  '@remix-run/web-fetch',
+  '@remix-run/web-blob',
+  '@remix-run/web-stream',
+  '@remix-run/web-form-data',
+  '@web3-storage/multipart-parser',
+  'preact',
+]);
+
 module.exports = (path, options) => {
   // Call the defaultResolver, so we leverage its cache, error handling, etc.
   return options.defaultResolver(path, {
     ...options,
     // Use packageFilter to process parsed `package.json` before the resolution (see https://www.npmjs.com/package/resolve#resolveid-opts-cb)
     packageFilter: (pkg) => {
-      // Currently, preact ships packages where the browser export condition targets ESM. This breaks Jest,
-      // because it prefers the browser condition but does not understand ESM yet. This deletes the
-      // offending package.json fields so that the CommonJS variants are preferred.
-      // @see https://github.com/microsoft/accessibility-insights-web/pull/5421#issuecomment-1109168149
-      if (pkg.name === 'preact') {
+      if (PACKAGES_WITH_ESM_BROWSER_ENTRIES.has(pkg.name)) {
         delete pkg['exports'];
         delete pkg['module'];
       }
