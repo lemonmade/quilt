0f2427c31358d1ce4c492a45f774cf88f750eb40
Chris Sauve <chrismsauve@gmail.com>
2023-02-14 18:24:34 -0500

    Add E2E tests

---
+++ tests/e2e/common/globals.ts
@@ -0,0 +1,54 @@
+import {createPerformance, type Performance} from '@quilted/performance';
+
+export interface Waiter {
+  wait(id: string): Promise<void>;
+  done(id: string): void;
+}
+
+declare global {
+  interface Window {
+    Quilt?: {
+      E2E?: {
+        Waiter?: Waiter;
+        Performance?: Performance;
+      };
+    };
+  }
+}
+
+export const performance = createPerformance();
+
+const waitingFor = new Map<string, Set<() => void>>();
+
+export const waiter: Waiter = {
+  wait(id) {
+    let waiters = waitingFor.get(id);
+
+    if (waiters == null) {
+      waiters = new Set();
+      waitingFor.set(id, waiters);
+    }
+
+    return new Promise((resolve) => {
+      waiters!.add(resolve);
+    });
+  },
+  done(id) {
+    const waiters = Array.from(waitingFor.get(id) ?? []);
+
+    for (const waiter of waiters) {
+      waiter();
+    }
+
+    waitingFor.delete(id);
+  },
+};
+
+// We expose this on a global so that our end-to-end tests can easily
+// run them.
+if (typeof window !== 'undefined') {
+  window.Quilt ??= {};
+  window.Quilt.E2E ??= {};
+  window.Quilt.E2E.Performance ??= performance;
+  window.Quilt.E2E.Waiter ??= waiter;
+}
