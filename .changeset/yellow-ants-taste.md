ed03b7a5658fd005912eca528f423df6ad650dae
Chris Sauve <chrismsauve@gmail.com>
2023-03-20 22:46:32 -0400

    Fix internal package aliases

---
+++ .changeset/yellow-ants-taste.md
@@ -0,0 +1,5 @@
+---
+'@quilted/craft': patch
+---
+
+Fix internal package aliases
--- packages/craft/source/plugins/alias-workspace-packages.ts
+++ packages/craft/source/plugins/alias-workspace-packages.ts
@@ -1,45 +1,59 @@
 import {createProjectPlugin} from '../kit';
 import type {Project, Workspace} from '../kit';
 
+import {addRollupNodeBundleInclusion} from '../tools/rollup';
 import type {} from '../tools/rollup';
 import type {} from '../tools/vite';
 
 import {sourceEntriesForProject} from '../features/packages';
 
 export function aliasWorkspacePackages() {
-  return createProjectPlugin({
-    name: 'Quilt.AliasWorkspacePackages',
-    develop({configure, project, workspace}) {
-      if (project.packageJson == null) return;
+  let internalSourceAliases: Promise<Record<string, string>> | undefined;
+  let namedInternalPackages: {name: string; project: Project}[] | undefined;
 
-      configure(({rollupPlugins, viteResolveAliases}) => {
+  return createProjectPlugin({
+    name: 'Quilt.Package.InternalConsumer',
+    develop({configure, workspace}) {
+      configure(({rollupPlugins, rollupNodeBundle, viteResolveAliases}) => {
         viteResolveAliases?.(async (aliases) => ({
           ...aliases,
-          ...(await getAliases(project, workspace)),
+          ...(await getAliases(workspace)),
         }));
+
+        rollupNodeBundle?.((bundle) =>
+          addRollupNodeBundleInclusion(
+            getNamedInternalPackages(workspace).map(({name}) => name),
+            bundle,
+          ),
+        );
 
         rollupPlugins?.(async (plugins) => {
           const {default: alias} = await import('@rollup/plugin-alias');
 
           return [
             alias({
-              entries: await getAliases(project, workspace),
+              entries: await getAliases(workspace),
             }),
             ...plugins,
           ];
         });
       });
     },
-    build({configure, project, workspace}) {
-      if (project.packageJson == null) return;
+    build({configure, workspace}) {
+      configure(({rollupPlugins, rollupNodeBundle}) => {
+        rollupNodeBundle?.((bundle) =>
+          addRollupNodeBundleInclusion(
+            getNamedInternalPackages(workspace).map(({name}) => name),
+            bundle,
+          ),
+        );
 
-      configure(({rollupPlugins}) => {
         rollupPlugins?.(async (plugins) => {
           const {default: alias} = await import('@rollup/plugin-alias');
 
           return [
             alias({
-              entries: await getAliases(project, workspace),
+              entries: await getAliases(workspace),
             }),
             ...plugins,
           ];
@@ -47,27 +61,46 @@ export function aliasWorkspacePackages() {
       });
     },
   });
-}
 
-async function getAliases(project: Project, workspace: Workspace) {
-  const aliases: Record<string, string> = {};
+  function getNamedInternalPackages(workspace: Workspace) {
+    if (namedInternalPackages != null) return namedInternalPackages;
 
-  for (const otherProject of workspace.projects) {
-    const name = otherProject.packageJson?.name;
+    namedInternalPackages = [];
 
-    if (name == null || !project.hasDependency(name, {all: true})) continue;
-
-    const packageEntries = await sourceEntriesForProject(otherProject);
-
-    // We sort the entries from longest to shortest so that more specific entries take
-    // precedence.
-    for (const [entry, source] of Object.entries(packageEntries).sort(
-      ([entryOne], [entryTwo]) => entryTwo.length - entryOne.length,
-    )) {
-      const aliasEntry = `${name}${entry === '.' ? '' : `/${entry.slice(2)}`}`;
-      aliases[aliasEntry] = source;
+    for (const otherProject of workspace.projects) {
+      const name = otherProject.packageJson?.name;
+      if (name != null) {
+        namedInternalPackages.push({name, project: otherProject});
+      }
     }
+
+    return namedInternalPackages;
   }
 
-  return aliases;
+  function getAliases(workspace: Workspace) {
+    internalSourceAliases ??= (async () => {
+      const aliases: Record<string, string> = {};
+
+      for (const {name, project: otherProject} of getNamedInternalPackages(
+        workspace,
+      )) {
+        const packageEntries = await sourceEntriesForProject(otherProject);
+
+        // We sort the entries from longest to shortest so that more specific entries take
+        // precedence.
+        for (const [entry, source] of Object.entries(packageEntries).sort(
+          ([entryOne], [entryTwo]) => entryTwo.length - entryOne.length,
+        )) {
+          const aliasEntry = `${name}${
+            entry === '.' ? '' : `/${entry.slice(2)}`
+          }`;
+          aliases[aliasEntry] = source;
+        }
+      }
+
+      return aliases;
+    })();
+
+    return internalSourceAliases;
+  }
 }
