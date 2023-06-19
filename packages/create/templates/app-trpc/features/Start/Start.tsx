197e558bfed2046344f99cfe2b1b28c31504100b
Chris Sauve <chrismsauve@gmail.com>
2023-06-19 18:50:22 -0400

    Fix export in tRPC template

--- packages/create/templates/app-trpc/features/Start/Start.tsx
+++ packages/create/templates/app-trpc/features/Start/Start.tsx
@@ -1,9 +1,16 @@
+import {usePerformanceNavigation} from '@quilted/quilt';
+
 import {trpc} from '~/shared/trpc.ts';
 
 import styles from './Start.module.css';
 
-export function Start() {
+export default function Start() {
   const [data] = trpc.message.useSuspenseQuery('world');
+
+  // This hook indicates that the page has loaded. It is used as part of Quilt’s
+  // navigation performance tracking feature. If you have disabled this feature,
+  // you can remove this hook.
+  usePerformanceNavigation();
 
   return <div className={styles.Start}>{data}</div>;
 }
