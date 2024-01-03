0a0b272a1b10a05dccc1aa275761f661d67addbb
Chris Sauve <chrismsauve@gmail.com>
2024-01-02 22:19:17 -0500

    Fix bundle excluding quilt/globals
    * Fix bundle excluding quilt/globals

--- tests/e2e/fixtures/basic-app/rollup.config.js
+++ tests/e2e/fixtures/basic-app/rollup.config.js
@@ -1,3 +1,8 @@
 import {quiltApp} from '@quilted/rollup/app';
 
-export default quiltApp();
+export default quiltApp({
+  assets: {
+    // Un-minified assets makes it easier to debug build outputs
+    minify: false,
+  },
+});
