5f77287d734a10af8f104f21ea48ff7afc183c22
Chris Sauve <chrismsauve@gmail.com>
2024-07-17 20:45:59 -0400

    Fix e2e config for vitest 2.0

--- configuration/vite.e2e.config.ts
+++ configuration/vite.e2e.config.ts
@@ -1,13 +1,17 @@
-import {defineConfig, configDefaults} from 'vitest/config';
+import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
   esbuild: {
-    // Without this, vitest preserves `using` statemenets, which aren’t supported in Node.
+    // Without this, vitest preserves `using` statements, which aren’t supported in Node.
     target: 'es2022',
+  },
+  server: {
+    watch: {
+      ignored: ['./tests/e2e/output/**'],
+    },
   },
   test: {
     testTimeout: 20_000,
     include: ['./tests/e2e/**/*.test.ts'],
-    watchExclude: [...configDefaults.watchExclude, './tests/e2e/output/**'],
   },
 });
