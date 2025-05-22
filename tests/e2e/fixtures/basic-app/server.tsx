3e3e030d4e55c24efa79698c3dd18c8b627a864c
Chris Sauve <chrismsauve@gmail.com>
2025-05-22 10:01:08 -0400

    Fix asset inlining for the default entry and add inline tests (#863)

--- tests/e2e/fixtures/basic-app/server.tsx
+++ tests/e2e/fixtures/basic-app/server.tsx
@@ -6,12 +6,14 @@ import {BrowserAssets} from 'quilt:module/assets';
 const router = new RequestRouter();
 const assets = new BrowserAssets();
 
-// For all GET requests, render our React application.
+// For all GET requests, render our Preact application.
 router.get(async (request) => {
   const [{default: App}, {renderAppToHTMLResponse}] = await Promise.all([
     import('./App.tsx'),
     import('@quilted/quilt/server'),
   ]);
+
+  assets.entry({id: './inline.css'}).styles;
 
   const response = await renderAppToHTMLResponse(<App />, {
     request,
